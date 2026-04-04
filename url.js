3f078c719d8c8e36c327f0b2fdeac0ae7dcffaec
LIN, YEN-CHIEH <113423063@cc.ncu.edu.tw>
2026-04-04 16:21:49 +0800

    Update URLs in url.js
    Replaced existing URLs with new form links.

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/a2BUHXTENaq4PtmA7',
+	'https://forms.gle/rwFvB5aogEMDZMo19',
 ];
